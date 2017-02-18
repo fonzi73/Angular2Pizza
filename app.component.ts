a78ddf249e487d47c7736c7635c2dfb2492b3635
Marco Fontana <fonzi@freenet.de>
2017-02-18 11:47:09 +0100

    new app.component

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+    selector: 'pizzaconection',
+    templateUrl: 'app/app.html',
+    styleUrls: []
+    
+})
+
+export class AppComponent {
+    
+}
